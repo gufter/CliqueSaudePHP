8e21270148e95e19599ea064e5f80bfe28bbc1d6
Gufter <gustavofq.1998@gmail.com>
2017-11-17 20:24:47 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Clique Saúde</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <header>
+            <h1>Clique Saude</h1>
+        </header>
+        <?php
+            include "menu.inc";
+        ?>
+    </body>
+</html>
